032f2a6910c715eafd15d1acadd84bac69b67f6a
pauljoda <pauldavis101@gmail.com>
2025-03-08 13:21:41 -0600

    Update flakes

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -1,17 +1,12 @@
-{ pkgs, ... }: 
-{
+{pkgs, ...}: {
   programs.vscode = {
     enable = true;
     package = pkgs.vscode;
     profiles.default.extensions = with pkgs.vscode-extensions; [
       # nix language
       bbenoist.nix
-      # nix-shell suport 
+      # nix-shell suport
       arrterian.nix-env-selector
-      # python
-      ms-python.python
-      # C/C++
-      ms-vscode.cpptools
 
       # Remote Login for servers
       ms-vscode-remote.remote-ssh
